3bce6e5c7be58a78994b79aacde77e270de189dd
renyimin <renyimin@renrenche.com>
2018-06-29 15:01:57 +0800

    消息确认示例

--- app/Http/Controllers/Demo/ConsumerConfirmController.php
+++ app/Http/Controllers/Demo/ConsumerConfirmController.php
@@ -39,24 +39,6 @@ class ConsumerConfirmController extends Controller
         $msg = new AMQPMessage('quit-cancel', ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT, 'content_type' => 'text/plain']);
         $channel->basic_publish($msg, 'consumerConfirmExt', 'routingkey', true, false);
 
-//        $set_ack_handler = function () {
-//          echo "set_ack_handle";
-//        };
-//        $channel->set_ack_handler($set_ack_handler);
-//
-//        $set_return_listener = function () {
-//            echo "set_return_listener";
-//        };
-//        $channel->set_return_listener($set_return_listener);
-//
-//        $set_nack_handler = function () {
-//            echo "set_nack_handler";
-//        };
-//        $channel->set_nack_handler($set_nack_handler);
-
-//        $channel->confirm_select();
-//        $channel->confirm_select_ok();
-
         $channel->close();
 
         $connection->close();
